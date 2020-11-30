97ae79f48dc09d87774ea00df13bde14098662b5
yeojin <yeojin@localhost.localdomain>
2020-12-01 01:46:35 +0900

    CreateAccountPage finalize

--- frontend/src/components/common/CreateAccountPage.vue
+++ frontend/src/components/common/CreateAccountPage.vue
@@ -63,7 +63,7 @@ export default {
                 password:'',
                 password_confirm:'',
                 name:'',
-                birthdate:'',
+                birthdate: '',
                 gender:'',
                 address:'',
                 phone:'',
