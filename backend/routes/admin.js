38b7f749a3e7848c8aa719d2216a05ffb1f52a2c
user <user@localhost.localdomain>
2020-12-01 23:28:49 +0900

    /admin/membermanage : 회원 리스트 출력 + 상세 정보 출력

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,30 +2,34 @@ var express = require('express');
 var router = express.Router();
 var mariadb = require('../mariadb');
 
-router.get('/membermanage', function(req, res, next) {
-    var response = {
-        success : false,
-        users : [],
-        message : ''
-    }
-
-    var list = []
-    
+router.get('/membermanage', function(req, res, next) {    
     mariadb.query(`SELECT ID, ROLE FROM ACCOUNT WHERE ROLE IN (\'ASE\', \'SUB\')`, function(err, rows, fields){
         if(!err){
-            response.success = true
             res.send(JSON.stringify(rows))
         } else {
-            response.message = "서버 오류입니다. 문제가 계속되는 경우 관리자에게 문의하세요.";
+            res.send(false)
         }
     });
 });
 
 router.get('/membermanage/:id', function(req, res, next){
-    console.log('params : '+req.params)
-    console.log('path : '+req.path)
-    console.log('query : '+req.query)
-    res.send(['awesome user'])
+    mariadb.query(`SELECT NAME, ID, BIRTHDATE, GENDER, ADDRESS, PHONE, ROLE, EVALUATION_GRADE FROM ACCOUNT WHERE ID=\'${req.params.id}\';`, function(err,rows,fields){
+        if(!err){
+            res.send(JSON.stringify(rows))
+        } else {
+            res.send(false)
+        }
+    });
+});
+
+router.get('/membermanage/:id/task', function(req, res, next){
+    mariadb.query(`SELECT TASK_NAME FROM PARTICIPATES_IN WHERE SUBMITEE_ID=\'${req.params.id}\';`, function(err, rows, fields){
+        if(!err){
+            res.send(JSON.stringify(rows))
+        } else {
+            res.send(false)
+        }
+    })
 })
 
 module.exports = router;
