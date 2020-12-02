cac698ba92b56cf30f81b6b086cc8d9886fb6aab
CM <cm0x0x0x0@gmail.com>
2020-12-03 02:22:56 +0900

    [평가자] 파싱 데이터 시퀀스 파일 평가 초안

--- backend/routes/submittee.js
+++ backend/routes/submittee.js
@@ -10,12 +10,12 @@ router.get('/tasklist', function(req, res, next) {
         message : ''
     }
 
-    mariadb.query(`SELECT NAME, MIN_UPLOAD_PERIOD ADMIN_ID FROM TASK`, function(err, rows, fields){
+    mariadb.query(`SELECT NAME, MIN_UPLOAD_PERIOD, ADMIN_ID FROM TASK`, function(err, rows, fields){
         if(!err){
             response.success = true
             res.send(JSON.stringify(rows))
         } else {
-            response.message = "서버 오류입니다. 문제가 계속되는 경우 관리자에게 문의하세요.";
+            res.send(false)
         }
         
     });
--- frontend/src/components/assessor/ParsingDataFileEvaluate.vue
+++ frontend/src/components/assessor/ParsingDataFileEvaluate.vue
@@ -1,20 +1,11 @@
-<!-- The page which shows a task list and receives a participation. -->
+<!-- The page which shows the parsing data sequence files allocated to user 
+and receives the qualitative evaluation of parsing data sequence files . -->
 <template>
 <div>
-    <h3>태스크 목록 및 참가신청 페이지</h3>
-    <div v-for="(task, index) in tasks" v-bind:key="task">
+    <h3>파싱 데이터 시퀀스 파일 정성평가 페이지</h3>
+    <div v-for="(file, index) in files" v-bind:key="file">
         <!--<h5>{{index + 1}} {{task.NAME}} <button v-on:click="apply(index)">신청하기</button></h5> -->
-        <h5>{{index + 1}} {{task.NAME}} 
-            <button @click="handle_toggle" type="button"> 신청하기 </button>
-
-            <div v-show="is_show">
-                <h5>개인정보 이용 동의서</h5>
-                <P>태스크에 참가신청을 하려면 개인정보 이용 동의를 해야합니다.</p>
-
-                <button v-on:click="apply(index)"> 동의 후 신청</button>
-            </div>
-        </h5>
-
+        <h5>{{index + 1}} {{file.ORIGINAL_DATA_TYPE_ID}} {{file.TASK_NAME}} {{file.TOTAL_TUPLE_NUM}} {{file.NULL_RATIO}} {{file.DUPLICATE_TUPLE_NUM}}</h5>
     </div>
 </div>   
 </template>
@@ -22,19 +13,19 @@
 <script>
 export default {
     created(){
-        if(this.$store.state.role!=='SUB'){
+        if(this.$store.state.role!=='ASE'){
             alert("권한이 없습니다")
             this.$router.go(-1)
         } else {
-            this.$http.get('/api/submittee/tasklist')
+            this.$http.get('/api/assessor/parsingevaluate')
             .then(res => {
-                this.tasks = res.data
+                this.files = res.data
             })
         }
     },
     data: function(){
         return{
-            tasks : [],
+            files : [],
             is_show : false
         }
     },
