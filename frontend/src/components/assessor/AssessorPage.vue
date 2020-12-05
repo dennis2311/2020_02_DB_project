a87d7f587e616494f2b10e91c110aba4e81d46b4
CM <cm0x0x0x0@gmail.com>
2020-12-05 22:55:26 +0900

    제출자 및 평가자 초기페이지 완성

--- frontend/src/components/assessor/AssessorPage.vue
+++ frontend/src/components/assessor/AssessorPage.vue
@@ -1,5 +1,8 @@
 <template>
 <div>
+    <router-link to='/assessor/parsingevaluate'>파싱 데이터 시퀀스 파일 평가</router-link> |
+    <router-link to='/assessor/monitorhistory'>평가 내역 모니터링</router-link> |
+    <router-link to='/assessor/evalgrademanage'>제출자 평가 점수 관리</router-link> |
     <router-view/>
 </div>
     
@@ -7,7 +10,13 @@
 
 <script>
 export default {
-    name:'AssessorPage'
+    created(){
+        if(this.$store.state.role!=='ASE'){
+            alert("권한이 없습니다")
+            this.$router.go(-1)
+        }
+    },
+    name:'AssessorPage',
 }
 </script>
 
